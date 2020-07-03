2cef8393306f98cba53afe41d637f31a9f667d3e
HafsaR <hafsa34343@gmail.com>
2020-07-03 10:08:00 -0600

    finish auth flow

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+mongoose.connect(keys.mongoURI);
+require("./models/users");
 
-app.get("/", (req, res) => {
-  res.send({ bye: "buddy!" });
-});
+require("./services/passport");
+
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+
+const app = express();
+require("./routes/authRoutes")(app);
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
